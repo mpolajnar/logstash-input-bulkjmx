de9400c047269b74c977ca6789a241a241090732
Matija Polajnar <matija.polajnar@marand.si>
2016-10-06 07:18:28 +0200

    Fix spec after plugin rename

--- spec/inputs/jmx_spec.rb
+++ spec/inputs/jmx_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
-require "logstash/inputs/jmx"
+require "logstash/inputs/bulk_jmx"
 require "logstash/codecs/plain"
 require 'stud/temporary'
 require "jmx4r"
